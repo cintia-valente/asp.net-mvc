02da7514b729b07033b545f55f3c8d8c0191dffd
cintia-valente <cintiavalente@live.com>
2023-11-23 15:32:24 -0300

    feature: início

---
+++ App/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+
+            home.Nome = "Cíntia Valente";
+            home.Email = "cintia@live.com";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
